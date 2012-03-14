b3239c2a867ae3d34dc1c93ede40bc6c62b6921a
Julien DIDIER <julien@jdidier.net>
2012-03-14 13:40:40 +0100

    Revert "Compiler pass to collect services tagged with buzz.client"
    This reverts commit 65a658bbf0e156af57c9398d4cb3f332405ca0d6.

--- BuzzProfilerBundle.php
+++ BuzzProfilerBundle.php
@@ -2,13 +2,8 @@
 
 namespace Buzz\Bundle\ProfilerBundle;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class BuzzProfilerBundle extends Bundle
-{
-    public function build(ContainerBuilder $container)
-    {
-        $container->addCompilerPass(new DependencyInjection\Compiler\DecorateBuzzClientPass());
-    }
+{    
 }
